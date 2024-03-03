775512d8f76d72b108b67fa5518bee63df2fb624
Claudio Silva Bispo <110735259+Claubis@users.noreply.github.com>
2024-03-03 18:41:38 -0300

    Add files via upload

--- Responsive Web Development/TSX/FOOTER/Footer.tsx
+++ Responsive Web Development/TSX/FOOTER/Footer.tsx
@@ -1,5 +1,10 @@
 import './styleFooter.css'
 
+import imagem1 from '../../assets/Navbar/logo2-removebg-preview.png'
+import imagem2 from '../../assets/Footer/endereco.png'
+import imagem3 from '../../assets/Footer/telefone.png'
+import imagem4 from '../../assets/Footer/calendario.png'
+
 export default function Footer() {
     
     return (
@@ -11,26 +16,28 @@ export default function Footer() {
             </div>
 
             <a className="footer-info footer-link" target="_blank" href="https://maps.app.goo.gl/aT8dW1a6WcWkLrUP8">
-                <img className="footer-icone" src="/IMG/FOOTER/endereco.png" alt="Ícone endereco" title="Ícone endereco"/>
-                <p>415 Mission Street 3rd F, San Francisco, CA 94105, United States</p>
+                <img className="footer-icone" src={imagem2} title="Ícone endereco"/>
+                <p>Av. Paulista, 1106 - 7º andar - Bela Vista, São Paulo - SP, 01311-000</p>
             </a>
 
             <div className="footer-info">
-                <img className="footer-icone" src="/IMG/FOOTER/telefone.png" alt="Ícone telefone" title="Ícone telefone" />
+                <img className="footer-icone" src={imagem3} title="Ícone telefone" />
                 <p>Cel: 0800 891 1887</p>
             </div>
 
             <a className="footer-info footer-link" target="_blank" href="index.html">
-                <img className="footer-icone" src="/IMG/FOOTER/calendario.png" alt="Ícone calendario" title="Ícone calendario" />
+                <img className="footer-icone" src={imagem4} title="Ícone calendario" />
                 <p>Agende uma consultoria para seu negócio</p>
             </a>
 
         </div>
 
         <div className="footer-copyright max-container"> 
-        <p>Tokito Techs </p>
+            <img  className="nav-wrapper--logo--Tokito" src={imagem1} alt="Logo da Tokito Techs" />
+            <p className='logo-tokito-techs'>Tokito Techs | Challenge SalesForce </p>
         </div>
-        </footer>
+
+    </footer>
     );
 
 }
