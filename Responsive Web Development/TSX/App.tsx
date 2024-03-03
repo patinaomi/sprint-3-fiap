b400563ded228af6d72905090bbd19ee2579dea3
Claudio Silva Bispo <110735259+Claubis@users.noreply.github.com>
2024-03-03 17:01:44 -0300

    Add files via upload

--- Responsive Web Development/TSX/App.tsx
+++ Responsive Web Development/TSX/App.tsx
@@ -1,12 +1,31 @@
-import Navbar from './Componentes/Menu/Navbar'
-import './CSS/Style_Navbar.css'
+import Navbar from './components/Menu/Navbar'
+import PrimeiraSessao from './components/PrimeiraSessao/PrimeiraSessao'
+import SegundaSessao from './components/SegundaSessao/SegundaSessao'
+import TerceiraSessao from './components/TerceiraSessao/TerceiraSessao'
+import QuartaSessao from './components/QuartaSessao/QuartaSessao'
+import QuintaSessao from './components/QuintaSessao/QuintaSessao'
+import Footer from './components/Footer/Footer'
+
+import NovaSessao from './components/NovaSessao/NovaSessao'
 
 export default function App() {
     return (
-        <div>
+        <>
             <Navbar/>
-                
-            
-        </div>
+
+            <PrimeiraSessao/>
+
+            <NovaSessao/>
+
+            <SegundaSessao/>
+
+            <TerceiraSessao/>
+
+            <QuartaSessao/>
+
+            <QuintaSessao/>
+
+            <Footer/>
+        </>
     )
 }
