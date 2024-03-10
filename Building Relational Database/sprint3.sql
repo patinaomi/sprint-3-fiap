a24762e04a410e0ebbb2325d48d918f666253c12
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-03-10 16:24:27 -0300

    Update and rename ex.sql to sprint3.sql

---
+++ Building Relational Database/sprint3.sql
@@ -0,0 +1,31 @@
+-- TOKITO TECHS
+-- RM553472 Claudio Bispo
+-- RM552918 Patricia Naomi
+-- RM554021 Sara Ingrid
+
+
+-- o que está como !!! tem que validar com o professor
+-- DUVIDAS
+-- 1. se os atributos tiverem q ser iguais ao da tabela, tem q alterar no DER
+-- 2. validar regras de negocio em relacao ao not null
+-- 3. validar nomes FK, fiz usando um padrao diferente, qual eh o certo??
+-- 4. estando tudo certo, tirar comentarios
+
+
+
+
+-- Verificar se a tabela já existe, caso exista será apagada junto com suas dependências
+DROP TABLE Visitante CASCADE CONSTRAINTS;
+
+-- Criação da tabela Visitante
+                        -- !!!verificar prof, auto increment
+CREATE TABLE Visitante(id_visit NUMBER(10) GENERATED ALWAYS as IDENTITY START WITH 1 INCREMENT BY 1
+                       CONSTRAINT visitante_id_nn NOT NULL
+                       CONSTRAINT visitante_id_pk PRIMARY KEY, 
+                       nome_visit VARCHAR2(30) CONSTRAINT visitante_nome_nn NOT NULL,
+                       email_visit VARCHAR2(40) CONSTRAINT visitante_email_nn NOT NULL,
+                       data_visit DATE CONSTRAINT visitante_data_nn NOT NULL,
+                       hora_visit VARCHAR2(8) CONSTRAINT visitante_hora_nn NOT NULL,
+                       -- !!! Armazenando como 'HH:MM:SS pois não tem TIME no Oracle
+                       tempo_visit NUMBER(10) CONSTRAINT visitante_tempo_nn NOT NULL)
+
