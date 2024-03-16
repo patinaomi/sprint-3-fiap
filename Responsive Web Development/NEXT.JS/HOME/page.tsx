5d68c3f8a9cfbba334faa1c4f8480481847aae11
Claudio Silva Bispo <110735259+Claubis@users.noreply.github.com>
2024-03-15 22:59:16 -0300

    Criação do componente Home
    Esse componente será nossa primeira tela com efeito no logo.

---
+++ Responsive Web Development/NEXT.JS/HOME/page.tsx
@@ -0,0 +1,37 @@
+
+/* Importação do CSS */
+import './style.css'
+
+/* Imortação das imagens */
+import imagem1 from './banner-img.jpg'
+import imagem2 from './logo2-removebg-preview.png'
+
+export default function HomePage(){
+    return (
+        
+        <div className="container-home-page">
+
+            <div className='logo-banner-content-pricipal'>
+                <img className="banner-imagem-principal" src={imagem1.src} alt="Banner"/>
+            </div> {/* Fim div logo-banner-content-pricipal */}
+
+            <div className='container-logos-tokito'>
+
+                <div className="logo-banner-content-tokito">
+                    <img className='logo-tokito' src={imagem2.src} alt="Logo da Tokito Techs" />
+                </div> {/* Fim div logo-banner-content-tokito */}
+
+                <div className='texto-logo-tokito-techs'>
+                    <h1 className='titulo-tokito-techs'>Tokito Techs</h1>
+                </div> {/* Fim div texto-logo-tokito-techs */}
+                    
+            </div> {/* Fim div container-logos-tokito */}
+
+            <div className='frase-efetio-home-page'>
+                <h2 className='frase-efeito'>Transformando Ideias em Realidade Digital.</h2>
+            </div> {/* Fim div frase-efetio-home-page */}
+
+        </div> /* Fim div containe-home-page*/
+
+    );
+}
