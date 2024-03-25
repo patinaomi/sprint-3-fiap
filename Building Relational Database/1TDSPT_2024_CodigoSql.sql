b68669feb82cb19f69d9dcd54d09c317456b3fe0
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-03-25 20:17:01 -0300

    finalização código

--- Building Relational Database/1TDSPT_2024_CodigoSql.sql
+++ Building Relational Database/1TDSPT_2024_CodigoSql.sql
@@ -1,45 +1,33 @@
 -- TOKITO TECHS
 -- RM553472 Claudio Bispo
--- RM552918 Patricia Naomi
+-- RM552981 Patricia Naomi
 -- RM554021 Sara Ingrid
-
-
--- o que está como !!! tem que validar com o professor
--- DUVIDAS
--- 1. se os atributos tiverem q ser iguais ao da tabela, tem q alterar no DER
--- 2. (JA VERIFICADO) validar regras de negocio em relacao ao not null
--- 3. (JA VERIFICADO) validar nomes FK, fiz usando um padrao diferente, qual eh o certo??
--- 4. estando tudo certo, tirar comentarios
--- 5. EU ACHEI muito pouco para os campos VARCHAR(20) etc
--- 6. pergunta chatbot está com duas primary key, ta errado isso não? 
--- então acabei assumindo que a unica PK seria pergunta_chatbot, nao entendo porque
--- resposta chatbot tem que ser PK também???
-
-
 
 
 -- Verificar se a tabela já existe, caso exista será apagada junto com suas dependências
 DROP TABLE Visitante CASCADE CONSTRAINTS;
+DROP TABLE Plataforma_Login CASCADE CONSTRAINTS;
+DROP TABLE Questionario CASCADE CONSTRAINTS;
+DROP TABLE Leads CASCADE CONSTRAINTS;
+DROP TABLE Pergunta_Chatbot CASCADE CONSTRAINTS;
+DROP TABLE Experiencia_Usuario CASCADE CONSTRAINTS;
+
+-- para verificar o padrão da data
+SELECT SYSDATE FROM DUAL;
+
 
 -- Criação da tabela Visitante
-                        -- !!!verificar prof, auto increment
 CREATE TABLE Visitante(id_visit NUMBER(10) GENERATED ALWAYS as IDENTITY START WITH 1 INCREMENT BY 1
-                       CONSTRAINT visitante_id_nn NOT NULL
                        CONSTRAINT visitante_id_pk PRIMARY KEY, 
                        nome_visit VARCHAR2(30) CONSTRAINT visitante_nome_nn NOT NULL,
                        email_visit VARCHAR2(40) CONSTRAINT visitante_email_nn NOT NULL,
                        data_visit DATE CONSTRAINT visitante_data_nn NOT NULL,
+                       -- será armazenado nesse padrão 14:30:00
                        hora_visit VARCHAR2(8) CONSTRAINT visitante_hora_nn NOT NULL,
-                       -- !!! Armazenando como 'HH:MM:SS pois não tem TIME no Oracle
                        tempo_visit NUMBER(10) CONSTRAINT visitante_tempo_nn NOT NULL);
-
-
--- Deleta a tabela Plataforma Login caso já exista
-DROP TABLE Plataforma_Login CASCADE CONSTRAINTS;
 
 -- Criação da Tabela Plataforma_Login
 CREATE TABLE Plataforma_Login(id_login NUMBER(10) GENERATED ALWAYS as IDENTITY START WITH 1 INCREMENT BY 1
-                              CONSTRAINT login_id_nn NOT NULL
                               CONSTRAINT login_id_pk PRIMARY KEY,
                               visit_id_visit_fk NUMBER(10) CONSTRAINT login_idvisit_nn NOT NULL,
                               nome_login VARCHAR2(40) CONSTRAINT login_nome_nn NOT NULL,
@@ -49,12 +37,8 @@ CREATE TABLE Plataforma_Login(id_login NUMBER(10) GENERATED ALWAYS as IDENTITY S
                               CONSTRAINT platlogin_visitante_fk FOREIGN KEY (visit_id_visit_fk) REFERENCES Visitante(id_visit));
 
 
--- Deleta a tabela Questionario caso já exista
-DROP TABLE Questionario CASCADE CONSTRAINTS;
-
 -- Criação da Tabela Questionario
 CREATE TABLE Questionario(num_quest NUMBER(10) GENERATED ALWAYS as IDENTITY START WITH 1 INCREMENT BY 1
-                          CONSTRAINT quest_num_nn NOT NULL
                           CONSTRAINT quest_num_pk PRIMARY KEY,
                           visit_id_visit_fk NUMBER(10) CONSTRAINT quest_idvisit_nn NOT NULL,
                           data_hora_visita_quest DATE CONSTRAINT quest_data_hora_visita_nn NOT NULL,
@@ -70,14 +54,10 @@ CREATE TABLE Questionario(num_quest NUMBER(10) GENERATED ALWAYS as IDENTITY STAR
                           email_quest VARCHAR2(40) CONSTRAINT quest_email_nn NOT NULL,
                           CONSTRAINT quest_visitante_fk FOREIGN KEY(visit_id_visit_fk) REFERENCES Visitante(id_visit));
                       
-                          
--- Deleta a tabela Leads caso necessário
-DROP TABLE Leads CASCADE CONSTRAINTS;
--- !!! coloquei leadS no plural pois Lead no sql é uma palavra reservada
 
--- Criação da tabela
+-- coloquei leadS no plural pois Lead no sql é uma função
+-- Criação da tabela Leads
 CREATE TABLE Leads(id_leads NUMBER(10) GENERATED ALWAYS as IDENTITY START WITH 1 INCREMENT BY 1
-                   CONSTRAINT leads_id_nn NOT NULL
                    CONSTRAINT leads_id_pk PRIMARY KEY,
                    quest_num_quest_fk NUMBER(10) CONSTRAINT leads_num_quest_nn NOT NULL,
                    platlog_id_login_fk NUMBER(10) CONSTRAINT leads_id_login_nn NOT NULL,
@@ -87,43 +67,46 @@ CREATE TABLE Leads(id_leads NUMBER(10) GENERATED ALWAYS as IDENTITY START WITH 1
                    data_hora_login_leads DATE CONSTRAINT leads_data_hora_login_nn NOT NULL,
                    CONSTRAINT leads_quest_fk FOREIGN KEY(quest_num_quest_fk) REFERENCES Questionario(num_quest),
                    CONSTRAINT leads_platlog_fk FOREIGN KEY(platlog_id_login_fk) REFERENCES Plataforma_Login(id_login));
-   
-   
--- Deleta a tabela Pergunta_Chatbot caso necessário
-DROP TABLE Pergunta_Chatbot CASCADE CONSTRAINTS;
 
--- Criação da tabela                    
-CREATE TABLE Pergunta_Chatbot(id_pergunta_chatbot NUMBER(10) GENERATED ALWAYS as IDENTITY START WITH 1 INCREMENT BY 1
-                              CONSTRAINT chatbot_idperg_nn NOT NULL
-                              CONSTRAINT chatbot_idperg_pk PRIMARY KEY,
-                              -- aqui onde fica minha duvida sobre as duas PK
-                              id_resp_chatbot NUMBER(10) CONSTRAINT chatbot_idresp_nn NOT NULL,
+-- Criação da tabela Pergunta Chatbot                    
+CREATE TABLE Pergunta_Chatbot(id_pergunta_chatbot NUMBER(10),
+                              id_resposta_chatbot NUMBER(10),
                               visit_id_visit_fk NUMBER(10) CONSTRAINT chatbot_idvisit_fk NOT NULL,
                               conteudo_chatbot VARCHAR2(300) CONSTRAINT chatbot_conteudo_nn NOT NULL,
-                              entidade_chatbot VARCHAR2(100),  -- !!!! Aqui deixei NULL pois entendo q n necessariamente ira obter esses dados
-                              intencao_chatbot VARCHAR2(35),
+                              entidade_chatbot VARCHAR2(100),
+                              intencao_chatbot VARCHAR2(50),
                               produto_chatbot VARCHAR2(100),
-                              satisfacao_chatbot VARCHAR2(10),
+                              satisfacao_chatbot VARCHAR2(30),
                               data_hora_chatbot DATE CONSTRAINT chatbot_data_hora_nn NOT NULL,
+                              PRIMARY KEY (id_pergunta_chatbot, id_resposta_chatbot),
                               CONSTRAINT perguntachatbot_visitante_fk FOREIGN KEY (visit_id_visit_fk) REFERENCES Visitante(id_visit));
-                                                     -- !!! tudo bem terem varias FK com o mesmo nome?
-                    
-   
--- Deleta a tabela Experiencia_Usuario caso necessário                              
-DROP TABLE Experiencia_Usuario CASCADE CONSTRAINTS;
+                              
 
--- Criação da Tabela
+-- Criação da Tabela Experiência Usuário
 CREATE TABLE Experiencia_Usuario(id_exp NUMBER(10)GENERATED ALWAYS as IDENTITY START WITH 1 INCREMENT BY 1
-                                 CONSTRAINT exp_id_nn NOT NULL
                                  CONSTRAINT exp_id_pk PRIMARY KEY,
                                  visit_id_visit_fk NUMBER(10) CONSTRAINT exp_id_visit_nn NOT NULL,
-                                 chatbot_id_pergunta_fk NUMBER(10) CONSTRAINT exp_id_pergunta_nn NOT NULL,
                                  data_hora_vis_exp DATE CONSTRAINT exp_data_hora_vis_nn NOT NULL,
-                                 duvida_exp VARCHAR2(300),
-                                 prod_pesquisado_exp VARCHAR2(30),
-                                 pag_visitada_exp VARCHAR2(30),
+                                 prod_pesquisado_exp VARCHAR2(50),
+                                 pag_visitada_exp VARCHAR2(50),
                                  tempo_visita_exp NUMBER(8) CONSTRAINT exp_tempo_visita_nn NOT NULL,
-                                    -- !!! Armazenando como 'HH:MM:SS
-                                 CONSTRAINT exp_visit_fk FOREIGN KEY(visit_id_visit_fk) REFERENCES Visitante(id_visit),
-                                 CONSTRAINT exp_pergunta_fk FOREIGN KEY (chatbot_id_pergunta_fk) REFERENCES Pergunta_Chatbot(id_pergunta_chatbot));
+                                 CONSTRAINT exp_visit_fk FOREIGN KEY(visit_id_visit_fk) REFERENCES Visitante(id_visit));
+            
                                                    
+-- TESTE INSERT
+insert into Visitante (nome_visit, email_visit, data_visit, hora_visit, tempo_visit) VALUES
+('Pati', 'pati@pi.com', '25/03/24', '14:30:00', 0034)
+
+INSERT INTO Plataforma_Login(visit_id_visit_fk, nome_login, status_login, data_implementacao_login, email_login) VALUES
+(1, 'patinaomi', 'Ativo', '25/03/25', 'pat@oi.com') 
+
+INSERT INTO Questionario(visit_id_visit_fk, data_hora_visita_quest, email_quest) VALUES
+(1, '22/03/24', 'pat@oi.com')
+
+INSERT INTO Leads(quest_num_quest_fk, platlog_id_login_fk, nome_leads, email_leads, empresa_leads, data_hora_login_leads) VALUES
+(1, 1, 'Claudio Bispo', 'clau@oi.com', 'Tokito', (TO_DATE('01/03/2024 14:30:00', 'DD-MM-YYYY HH24:MI:SS')));
+
+INSERT INTO Pergunta_Chatbot VALUES (1, 1, 1, 'Lorem ipsum', 'Entidade', 'Intenção', 'Produto', 'Satisfeito', '20/03/24');
+
+INSERT INTO Experiencia_Usuario(visit_id_visit_fk, data_hora_vis_exp, prod_pesquisado_exp, pag_visitada_exp, tempo_visita_exp)
+VALUES (1, '20/03/24', 'Tableau', 'Contato', 23);
