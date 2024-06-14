b3a1ffd50cb08d24efddc7221cacf44272f60c2a
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-06-14 18:29:13 -0300

    Delete Domain Driven Design/sprint-4-ddd/projetoTeste/src/main/java/br/com/fiap/model/dao/ConsultaProdutoDao.java

--- Domain Driven Design/sprint-4-ddd/projetoTeste/src/main/java/br/com/fiap/model/dao/ConsultaProdutoDao.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.model.dao;
-
-import br.com.fiap.model.vo.ConsultaProduto;
-import java.sql.SQLException;
-import java.util.List;
-
-public interface ConsultaProdutoDao {
-    void inserir(ConsultaProduto consultaProduto) throws SQLException;
-}
